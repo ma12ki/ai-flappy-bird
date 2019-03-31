917d3c3323a6a98e6412335e827516ad5cea61eb
Marcin Zolkiewski <zolkiewski.marcin@gmail.com>
2019-03-31 19:28:18 +0200

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+// var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 // Phaser webpack config
-var phaserModule = path.join(__dirname, '/node_modules/phaser/');
-var phaser = path.join(phaserModule, 'src/phaser.js');
+// var phaserModule = path.join(__dirname, '/node_modules/phaser/');
+// var phaser = path.join(phaserModule, 'src/phaser.js');
 
 var definePlugin = new webpack.DefinePlugin({
     __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
@@ -66,14 +66,4 @@ module.exports = {
             { test: [/\.vert$/, /\.frag$/], use: 'raw-loader' },
         ],
     },
-    /* node: {
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty'
-    },
-    resolve: {
-        alias: {
-            'phaser': phaser,
-        }
-    }*/
 };
